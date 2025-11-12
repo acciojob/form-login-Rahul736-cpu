9bde440471e5442069e0d547f000da5d462b9cb6
Accio-1 <rahulwanjare291@gmail.com>
2025-11-12 15:21:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
+function getFormvalue(event) {
+	
     //Write your code here
+
+	event.preventDefault();
+
+	const firstName=document.forms["form1"]["fname"].value;
+	const lastname=document.forms["form2"]["lname"].value;
+
+	alert(firstName+" "+lastname);
+	
+
 
 }
